27d4f494d5421a345e164333c090aca864d374f2
Latiful Ahad <sabitman@hotmail.com>
2019-04-12 19:19:52 -0400

    Rendered ADT with deterministic prin.

--- Immersive_Course_Folders/W2/Hash_LRU_LL/lib/p03_hash_set.rb
+++ Immersive_Course_Folders/W2/Hash_LRU_LL/lib/p03_hash_set.rb
@@ -7,24 +7,34 @@ class HashSet
   end
 
   def insert(key)
+    self[key] << key
+    @count += 1
+    resize! if count > @store.size
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      @count -= 1
+      self[key].delete(key)
+    end
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[(num.hash % num_buckets)]
   end
 
   def num_buckets
     @store.length
   end
 
-  def resize!
+  def resize! 
+    8.times { @store << Array.new }
+    @store.flatten.each { |el| self[el] << el }
   end
 end
