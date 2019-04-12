8a6ab238116cf694c00394bab9eade0d3325b954
Latiful Ahad <sabitman@hotmail.com>
2019-04-12 18:41:45 -0400

    Created basic h_func. < pathological data

--- Immersive_Course_Folders/W2/Hash_LRU_LL/lib/p02_hashing.rb
+++ Immersive_Course_Folders/W2/Hash_LRU_LL/lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    length, val, range = self.size, 0, (self[0] - self[-1]).abs unless self.empty?
+    self.each { |el| val += el }
+    self.empty? ? (1).hash : (length + val + range).hash 
   end
 end
 
 class String
   def hash
+    letters, length = self.chars.map(&:ord), self.size
+    disparity = letters[0] + letters[-1]
+    (length + disparity).hash
   end
 end
 
@@ -16,6 +22,9 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
+    # self.keys.sort_by(&:hash).hash 
+    # to_a.sort_by(&:hash).hash
     0
+    self.keys.size.hash
   end
 end
