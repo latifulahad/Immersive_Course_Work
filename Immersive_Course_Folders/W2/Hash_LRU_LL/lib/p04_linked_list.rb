09cab0d00890b72eb1d1132aa84cae09c53df6bc
Latiful Ahad <sabitman@hotmail.com>
2019-04-12 21:23:19 -0400

    Made LL w/all stipulations

--- Immersive_Course_Folders/W2/Hash_LRU_LL/lib/p04_linked_list.rb
+++ Immersive_Course_Folders/W2/Hash_LRU_LL/lib/p04_linked_list.rb
@@ -14,45 +14,71 @@ class Node
   end
 
   def remove
-    # optional but useful, connects previous link to next link
-    # and removes self from list.
+    @prev.next = @next
+    @next.prev = @prev
   end
 end
 
 class LinkedList
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
-    each_with_index { |link, j| return link if i == j }
+    nodes.each { |nd| return nd if nd.val == i }
     nil
   end
 
   def first
+    @head
   end
 
   def last
+    @tail
   end
 
-  def empty?
+  def empty? #Works
+    @tail.prev == @head
   end
 
-  def get(key)
+  def get(key) #Works
+    nodes.each { |nd| return nd.val if nd.key == key }
+    nil
   end
 
-  def include?(key)
+  def include?(key) #Works
+    nodes.each { |nd| return true if nd.key == key }
+    false
   end
 
-  def append(key, val)
+  def append(key, val) #Works
+    a = Node.new(key, val)
+    a.next = last
+    a.prev = last.prev
+    last.prev.next = a
+    last.prev = a
   end
 
-  def update(key, val)
+  def update(key, val) #Works
+    nodes.each { |nd| nd.val = val if nd.key == key }
   end
 
-  def remove(key)
+  def remove(key) #Works
+    nodes.each { |nd| nd.remove if nd.key == key }
   end
 
-  def each
+  def nodes #Works
+    nxt = @head.next
+    arr = [nxt]
+
+    until arr.include?(@tail)
+      arr << arr[-1].next
+    end
+
+    arr   
   end
 
   # uncomment when you have `each` working and `Enumerable` included
@@ -60,3 +86,22 @@ class LinkedList
   #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
   # end
 end
+                                                                                
+
+# a = LinkedList.new
+# p a.first #W
+# p a.last  #W
+
+# a.append(:second, 2) #W
+# a.append(:third, 3)
+# p a.empty? #W
+# p a.include?(:second) #W
+
+# a.update(:second, 5) #W
+# p a.get(:second) #W
+
+# a.remove(:third) #W
+# p a.last.prev.val
+
+# p a[3] #W
+# p a.get(:third)
