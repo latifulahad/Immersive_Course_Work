87c6dbc37c4377977e72515decf4e7f78b447a8f
Latiful Ahad <sabitman@hotmail.com>
2019-04-15 15:47:01 -0400

    Made LRUcache w/hash && LL

--- Immersive_Course_Folders/W2/Hash_LRU_LL/lib/p06_lru_cache.rb
+++ Immersive_Course_Folders/W2/Hash_LRU_LL/lib/p06_lru_cache.rb
@@ -2,34 +2,60 @@ require_relative 'p05_hash_map'
 require_relative 'p04_linked_list'
 
 class LRUCache
+  attr_reader :map
   def initialize(max, prc)
-    @map = HashMap.new
-    @store = LinkedList.new
+    @map = HashMap.new #my_interpretation would have been {} 
+    @store = LinkedList.new #my_inter ll => { node_obj-node_obj-node_obj }
     @max = max
     @prc = prc
   end
-
+  
   def count
     @map.count
   end
 
   def get(key)
+   if @map.include?(key)
+    update_node!(key)
+   else
+     @map.set(key, true)
+     @store.append(key, calc!(key))
+     @map.count > @max ? eject! : true
+   end
   end
 
   def to_s
     'Map: ' + @map.to_s + '\n' + 'Store: ' + @store.to_s
   end
 
+  def my_to_s
+    @store.each
+  end
+
   private
 
   def calc!(key)
-    # suggested helper method; insert an (un-cached) key
+    @prc.call(key)
   end
 
   def update_node!(node)
-    # suggested helper method; move a node to the end of the list
+    @store.remove(node)
+    @store.append(node, calc!(node))
   end
 
   def eject!
+    @store.remove(@store.first.key)
   end
+
 end
+
+prc = Proc.new { |key| key ** 2 } 
+a = LRUCache.new(4, prc)
+a.get(2)
+a.get(3)
+a.get(4)
+a.get(5)
+a.get(2)
+a.get(6)
+p a.map.count
+p a.my_to_s
