eb6f52c35a08966381e1d30d0e30871950c65bf1
Latiful Ahad <sabitman@hotmail.com>
2019-05-15 18:15:38 -0400

    Finished task,veryTwistedLogic4SetupAssoc.

--- Immersive_Course_Folders/W3/R.exer_skel./app/models/course.rb
+++ Immersive_Course_Folders/W3/R.exer_skel./app/models/course.rb
@@ -1,2 +1,22 @@
 class Course < ApplicationRecord
+  belongs_to :instructor,
+  primary_key: :id,
+  foreign_key: :instructor_id,
+  class_name: :User
+
+  has_many :enrollments,
+  primary_key: :id,
+  foreign_key: :course_id,
+  class_name: :Enrollment
+
+  has_many :prerequisite,
+  primary_key: :prereq_id,
+  foreign_key: :id,
+  class_name: :Course
+
+  has_many :enrolled_students,
+  through: :enrollments,
+  source: :user
 end
+# Finally, add an instructor association to Course. This will point to a User object. 
+# Note that Course is now related to User in two ways: instructor and enrolled_students.
--- Immersive_Course_Folders/W3/R.exer_skel./app/models/user.rb
+++ Immersive_Course_Folders/W3/R.exer_skel./app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  has_many :enrollments,
+  primary_key: :id,
+  foreign_key: :student_id,
+  class_name: :Enrollment
+
+  has_many :courses,
+  through: :enrollments,
+  source: :course
 end
+
+# Add associations for enrollments and enrolled_courses. This might take a little bit of thinking.
+
+# You will know you have succeeded when you can execute User.first.enrollments and 
+# User.first.enrolled_courses in the rails console. These commands should return the 
+# user's enrollments and enrolled courses.
