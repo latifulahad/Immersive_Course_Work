ab469e456c4c51b6707a671f8cb66c01dbabd4a8
Latiful Ahad <sabitman@hotmail.com>
2019-06-11 01:28:04 -0400

    Completed my_ActiveR via multi-facited classes

--- Immersive_Course_Folders/W3/my_vr_of_AcRec./lib/03_associatable.rb
+++ Immersive_Course_Folders/W3/my_vr_of_AcRec./lib/03_associatable.rb
@@ -3,48 +3,69 @@ require 'active_support/inflector'
 
 # Phase IIIa
 class AssocOptions
-  attr_accessor(
-    :foreign_key,
-    :class_name,
-    :primary_key
-  )
-
+  attr_accessor :foreign_key, :class_name, :primary_key
+  
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name #the table_name # is goin_2_get_pulled from SQLObject...
   end
 end
 
 class BelongsToOptions < AssocOptions
+
   def initialize(name, options = {})
-    # ...
+    defaults = {
+      primary_key: :id,
+      foreign_key: "#{name}_id",
+      class_name: name.to_s.camelcase
+    }
+
+    defaults.keys.each { |k| self.send("#{k}=", options[k] || defaults[k]) } 
   end
 end
 
 class HasManyOptions < AssocOptions
+
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {        
+      primary_key: :id,
+      foreign_key: "#{self_class_name.underscore}_id".to_sym, 
+      class_name: name.to_s.singularize.camelcase
+    }
+
+    defaults.keys.each { |k| self.send("#{k}=", options[k] || defaults[k]) }
   end
 end
 
 module Associatable
-  # Phase IIIb
-  def belongs_to(name, options = {})
-    # ...
-  end
 
+  def belongs_to(name, options = {})
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      options = self.assoc_options[name]
+      options.model_class.where(id: => options[:foreign_key])
+    end
+  end
+  
   def has_many(name, options = {})
-    # ...
+    self.assoc_options[name] = HasManyOptions.new(name, options)
+
+    define_method(name) do
+      options = self.assoc_options[name]
+      options.model_class.where(options[:foreign_key] => options[:primary_key])
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
-  end
+    @assoc_options ||= {}
+    @assoc_options          #this is a module @varible being declared that is individual btw MODELS due to 1@ !2
+  end                       #These are class lvl despite the lack of sel.assoc_opt due 2 extend's effect....
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
--- Immersive_Course_Folders/W3/my_vr_of_AcRec./lib/04_associatable2.rb
+++ Immersive_Course_Folders/W3/my_vr_of_AcRec./lib/04_associatable2.rb
@@ -2,9 +2,35 @@ require_relative '03_associatable'
 
 # Phase IV
 module Associatable
-  # Remember to go back to 04_associatable to write ::assoc_options
-
+  
   def has_one_through(name, through_name, source_name)
-    # ...
+    define_method(name) do
+      thru_obj = self.assoc_options[through_name]
+      sc_nm_obj = thru.model_class.assoc_options[source_name]
+
+      tru_table_nm = thru_obj.table_name #this wrks due to opt_class < AssocO which hold this #
+      tru_pk = thru_obj.primary_key
+      tru_fk = thru_obj.foreign_key
+
+      sc_table_nm = sc_nm_obj.table_name
+      sc_pk = sc_nm_obj.primary_key
+      sc_fk = sc_nm_obj.foreign_key
+
+      ans = DBConnection.execute(<<-SQL, tru_fk)
+      SELECT 
+        #{sc_table_nm}.*
+      FROM
+        #{tru_table_name} --b table
+      JOIN
+        #{sc_table_nm} --c table
+      ON 
+        #{tru_table_nm}.#{sc_fk} = #{sc_table_nm}.#{sc_pk}
+      WHERE
+        #{tru_table_nm}.#{tru_pk} = ? --a table linked via b's f_key
+      SQL
+
+      self.parse_all(ans)
+    end
   end
+
 end
