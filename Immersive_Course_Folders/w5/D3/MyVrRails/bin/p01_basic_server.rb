dde61ad139b42f5f0996a12ea039c1f0353b21dc
Latiful Ahad <sabitman@hotmail.com>
2019-08-21 17:57:14 -0400

    Wrote Primitive Rack_Obj

---
+++ Immersive_Course_Folders/w5/D3/MyVrRails/bin/p01_basic_server.rb
@@ -0,0 +1,15 @@
+require 'rack'
+
+prac_app = Proc.new do |env|
+    req = Rack::Request.new(env)
+    res = Rack::Response.new
+
+    res['Content-Type'] = 'text/html'
+    res.write(req.path)
+    res.finish
+end
+
+Rack::Server.start(
+    app: prac_app,  
+    Port: 3000
+)
