dbd41f3c233abca21af0d43e5f09c1e3fedac2e9
Latiful Ahad <sabitman@hotmail.com>
2019-08-23 22:45:46 -0400

    Wrote lower lvl metaD 4 my_vr_rails

--- Immersive_Course_Folders/w5/D3/MyVrRails/lib/controller_base.rb
+++ Immersive_Course_Folders/w5/D3/MyVrRails/lib/controller_base.rb
@@ -4,37 +4,52 @@ require 'erb'
 require_relative './session'
 
 class ControllerBase
-  attr_reader :req, :res, :params
+  attr_reader :req, :res, :params, :session
 
-  # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, route_params = {})
+    @req, @res = req, res
+    @already_built_response = []
   end
 
-  # Helper method to alias @already_built_response
-  def already_built_response?
+  def already_built_response?(content)
+    if @already_built_response.include?(content)
+      raise "Double Render" 
+    else
+      true
+    end
   end
 
-  # Set the response status code and header
   def redirect_to(url)
+    @res.status = 302
+    @res['Location'] = url
+    @session.store_session(@res)
   end
 
-  # Populate the response with content.
-  # Set the response's content type to the given type.
-  # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    already_built_response?(content)
+
+    @already_built_response << content
+    @res.write(content)
+    @res['Content-Type'] = content_type
+    @session.store_session(@res)
   end
 
-  # use ERB and binding to evaluate templates
-  # pass the rendered html to render_content
   def render(template_name)
+    dir_path = File.dirname(__FILE__)
+
+    fl_nm = File.join( dir_path, "..", "views", self.class.name.underscore, "#{template_name}.html.erb")
+
+    content = File.read(fl_nm)
+
+    render_content(ERB.new(content).result(binding),"text/html")
   end
 
-  # method exposing a `Session` object
   def session
+    @session ||= Session.new(req)
   end
 
-  # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
+    #Im instructed to call #render, but constraint to varify dups are already present within render_cont...
   end
 end
-
