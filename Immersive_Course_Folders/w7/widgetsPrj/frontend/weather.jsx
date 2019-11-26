100d55435bda07e25770e431d2713d32f2a2813f
Latiful Ahad <sabitman@hotmail.com>
2019-11-25 19:09:22 -0500

    Prac more comperhensive feat setup w/React

---
+++ Immersive_Course_Folders/w7/widgetsPrj/frontend/weather.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+const toQueryString = (obj) => {
+    const parts = [];
+    for (let i in obj) {
+        if (obj.hasOwnProperty(i)) {
+            parts.push(`${encodeURIComponent(i)}=${encodeURIComponent(obj[i])}`);
+        }
+    }
+    return parts.join('&');
+}
+
+class Weather extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { weather: "" };
+    }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(this.displayLoc.bind(this));
+    }
+
+    displayLoc(locat) {
+        const a = locat.coords.latitude;
+        const b = locat.coords.longitude; 
+        const para = {lat: a, lon: b};
+        let url = 'http://api.openweathermap.org/data/2.5/weather?';
+        url += toQueryString(para);
+        let ndedKey = 'c5e363f20f12b70a84c02462aa21bf39';
+        url += `&APPID=${ndedKey}`;
+        
+        const reqObj = new XMLHttpRequest();
+        
+        reqObj.open('GET', url, true)
+        reqObj.onreadystatechange = (evt) => {
+            if (reqObj.status === 200 && reqObj.readyState === XMLHttpRequest.DONE) {
+                const data = JSON.parse(reqObj.responseText);
+                this.setState({weather: data});
+             } 
+        };
+
+        reqObj.send();
+    }
+    
+    render() {
+        let content = <div></div>;
+        if (this.state.weather) {
+            const weather = this.state.weather;
+            const temp = (weather.main.temp - 273.15) * 1.8 + 32;
+
+            content = <div>
+                <p>{weather.name}</p>
+                <p>{temp.toFixed(1)} degrees</p>
+            </div>;
+        } else {
+            content = <div>loading weather...</div>;
+        }
+
+        return (
+            <div>
+                <h1>Weather</h1>
+                <div>{content}</div>
+            </div>
+        );
+
+    }
+}
+
+export default Weather;
--- Immersive_Course_Folders/w7/widgetsPrj/frontend/widget.jsx
+++ Immersive_Course_Folders/w7/widgetsPrj/frontend/widget.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Clock from './clock';
 import Tabs from './tabs';
+import Weather from './weather';
 
 class Widget extends React.Component {
     constructor(props) {
@@ -18,7 +19,8 @@ class Widget extends React.Component {
         return(
             <div>
                 <Clock />
-                <Tabs panes={this.panes}/>        
+                <Tabs panes={this.panes}/> 
+                <Weather /> 
             </div>
         )
     }
