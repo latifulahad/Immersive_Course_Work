290f68c734008365e1c7bc965f0111c5b2e99e9a
Latiful Ahad <sabitman@hotmail.com>
2019-12-24 12:08:11 -0500

    Complete store w/enhancer(selfMd)

--- Immersive_Course_Folders/Homeworks/Middlew_Redux/frontend/todo_redux.jsx
+++ Immersive_Course_Folders/Homeworks/Middlew_Redux/frontend/todo_redux.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
+    let store = configureStore(preloadedState);
 
+    // store.dispatch = addLoggingToDispatch(store); Phase I
+    // store = applyMiddlewares(store, addLoggingToDispatch); // Phase II
+  
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
+
+// const addLoggingToDispatch = (store) => {
+//   let dis = store.dispatch;
+
+//   return (action) => {
+//     console.log(store.getState());
+//     console.log(action);
+//     dis(action);
+//     console.log(store.getState());
+//   }
+// };  //INTERNAL WORKINGS OF A TYPICAL Logging type func......
+
+// const addLoggingToDispatch = store => next => action => {
+//   console.log(store.getState());
+//   console.log(action);
+//   next(action);
+//   console.log(store.getState());
+// }; //myVersion of redux's logger
+
+
+// const applyMiddlewares = (store, ...middlewares) => {
+//   let dispatch = store.dispatch;
+
+//   middlewares.forEach((midW) => {
+//     dispatch = midW(store)(dispatch);
+//   });
+
+//   return Object.assign({}, store, { dispatch });
+// }; //my version of redux's applyMiddleware(enhancer1, enhancer2, etc);
