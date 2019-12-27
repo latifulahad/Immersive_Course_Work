763800c9d915be80d63793d6f15b0fddac6eb813
Latiful Ahad <sabitman@hotmail.com>
2019-12-26 19:30:27 -0500

    Learning api configInBack4R/Redux nds

---
+++ Immersive_Course_Folders/w7/TodoP3/app/controllers/api/todos_controller.rb
@@ -0,0 +1,42 @@
+class Api::TodosController < ApplicationController 
+
+    def show
+        render json: Todo.find(params[:id])
+    end
+
+    def index
+        render json: Todo.all
+    end
+
+    def create
+        @todo = Todo.new(todo_params)
+        if @todo.save
+            render json: @todo
+        else
+            render json: @todo.errors.full_messages, status: 422
+        end
+    end
+
+    def update  
+        @wntedTodo = Todo.find(params[:id])
+        @wntedTodo.update(wanted_params)
+        
+        render json: @wntedTodo
+    end
+
+    def destroy
+        @todo = Todo.find(params[:id])
+        if @todo
+            @todo.delete
+            render json: @todo
+        else
+            render text: "Todo is not present"
+        end
+    end
+
+    private
+
+    def wanted_params
+        params.require(:todo).permit(:title, :body, :done) 
+    end
+end
