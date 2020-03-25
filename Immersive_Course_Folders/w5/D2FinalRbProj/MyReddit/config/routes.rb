bb6e1fa3cf2270237b20351fc9c087a074daa62a
Latiful Ahad <sabitman@hotmail.com>
2020-03-24 23:35:40 -0400

    adding frntEnd 2 MyReddit

--- Immersive_Course_Folders/w5/D2FinalRbProj/MyReddit/config/routes.rb
+++ Immersive_Course_Folders/w5/D2FinalRbProj/MyReddit/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'static_pages#root'
+  resources :static_pages, only: [:root]
+
   resources :users, only: [:new, :show, :create, :edit, :update, :destroy]
 
   resources :subs, only: [:index, :new, :show, :create, :edit, :update] do
---
+++ Immersive_Course_Folders/w5/D2FinalRbProj/MyReddit/frontend/index.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    let wntTag = document.getElementById("root")
+
+    ReactDOM.render(<p>Hey There</p>, wntTag)
+})
