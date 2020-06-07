1221e83f04a20d5569ddca36d1d903277e85acab
Latiful Ahad <sabitman@hotmail.com>
2020-06-06 22:34:03 -0400

    fixtureDefect mitigated

--- Immersive_Course_Folders/w5/D2FinalRbProj/MyReddit/spec/models/user_spec.rb
+++ Immersive_Course_Folders/w5/D2FinalRbProj/MyReddit/spec/models/user_spec.rb
@@ -1,13 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  subject(:usr) { User.new(name: "Nafiz", email: "nafiz@aol.com", password: "nafizman3") }
+
   context "Validations tests" do 
-    
+    it "Name is attr is provided" do
+      user = User.new(email: "sabitman@aol.com").save
+      expect(user).to eq(false)
+    end
+
+    it "Email is unique" do
+      user1 = User.new(name: "Rash", email: "suravi@aol.com", password: "rash143").save
+      expect { User.new(name: "Sabit", email: "suravi@aol.com", password: "sabit143").save }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it "Password length is < 4 char" do
+      user =  User.new(name: "Omar", email: "omar@aol.com", password: "sabitman3").save
+      expect(user).to eq(true)
+    end
   end
 
-  context "Scope tests" do 
+  context "Business Logic" do
 
+    it "#reset_session_tkn!" do
+      usr.reset_session_tkn!
+      dataEtr = usr.session_token
+      expect(dataEtr).to_not be_empty
+    end
+
+    it "#is_pass?" do
+      expect(usr.is_pass?("nafizman3")).to eq(true)
+    end
   end
-  
+
 end
